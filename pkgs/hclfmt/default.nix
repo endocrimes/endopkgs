3f3d96029044d9d32b441c9456e13f8ce73f9bc9
Danielle Lancashire <dani@builds.terrible.systems>
2019-07-20 16:43:23 +0200

    hclfmt: Fix deps

--- pkgs/hclfmt/default.nix
+++ pkgs/hclfmt/default.nix
@@ -1,18 +1,18 @@
-{ stdenv, buildGoModule, fetchFromGitHub }:
+{ stdenv, buildGoPackage, fetchFromGitHub }:
 
-buildGoModule rec {
+buildGoPackage rec {
   name = "hclfmt-${version}";
   version = "0.1.0";
-
   goPackagePath = "github.com/fatih/hclfmt";
 
   src = fetchFromGitHub {
-    owner  = "fatih";
-    repo   = "hclfmt";
-    rev    = "374cd0fd2a551b57d2538b8f48d63e54fb86468b";
+    owner = "fatih";
+    repo = "hclfmt";
+    rev = "374cd0fd2a551b57d2538b8f48d63e54fb86468b";
+    sha256 = "0i0r6hdp95x1dakm37p3djd60xz1zbz71zq419w66465pd8k5vxw";
   };
 
-  subPackages = [ "." ];
+  goDeps = ./deps.nix;
 
   meta = with stdenv.lib; {
     description = "A formatter for the HashiCorp Configuration Language.";
